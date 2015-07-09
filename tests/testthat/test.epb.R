c5ed720c09939a12a6b22f1f0a0fccdf8f5a7977
Josh Errickson <jerrick@umich.edu>
2015-07-09 13:15:15 -0400

    Updated epb() to follow 36ab31f
    This is why we don't have blank tests...

--- tests/testthat/test.epb.R
+++ tests/testthat/test.epb.R
@@ -1,1 +1,11 @@
 context("epb")
+
+test_that("epb", {
+  # Need to expand these a LOT
+
+  resp <- rnorm(10)
+  covs <- data.frame(x1=rnorm(10), x2=rnorm(10))
+  treatment <- rep(0:1, each=5)
+
+  epb(resp, covs, treatment)
+})
