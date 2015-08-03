572b1dee719b6807988c98d52b3a23363a44a5f4
Josh Errickson <josh@errickson.net>
2015-08-03 13:57:42 -0400

    Expanded tests for pblm

--- tests/testthat/test.pblm.R
+++ tests/testthat/test.pblm.R
@@ -1,28 +1,49 @@
 context("pblm")
 
-test_that("pblm", {
-  # Need to expand these a LOT
+test_that("making pblm object", {
+  s <- lm(1~1)
+  s <- as(s, "pblm")
+  expect_true(is(s, "pblm"))
+  expect_true(is(s, "lm"))
+  # These tests are likely tautologies, but assigning `s` to class
+  # `pblm` could error.
+})
 
-  # Testing valid CI's (have to set seed because this data is
-  # terribly fit, so its going to be mostly Inf)
-  set.seed(134)
-  x1 <- rnorm(10)
-  x2 <- rnorm(10)
-  y <- x1+x2+rnorm(10)
-  d <- data.frame(y,x1,x2)
-  rm(y,x1,x2)
-  treatment <- rep(0:1, each=5)
+test_that("pblm input/output", {
+  d <- data.frame(abc=rnorm(10),
+                  x=rnorm(10),
+                  z=rnorm(10))
+  t <- rep(0:1, each=5)
 
-  mod1 <- lm(y ~ ., data=d, subset=treatment==0)
+  mod1 <- lm(abc ~ x + z, data=d, subset=t==0)
 
-  e <- pblm(mod1, treatment, d)
-  se <- summary(e)
+  e <- pblm(mod1, t, d)
 
   expect_true(is(e, "pblm"))
-  expect_true(is(e, "lm"))
-  expect_true(is(se, "summary.lm"))
+  expect_equal(e, as(e, "lm"), check.attributes=FALSE)
 
-  # Check NA's
-  expect_true(all(is.na(se$coefficients[2, 3:4])))
+  expect_true(length(coef(e)) == 2)
+  expect_equal(names(coef(e)), c("treatment", "pred"))
 
+  # These are tested explicitly in makemod2, but checking that they
+  # don't get overwritten when converting to pblm.
+
+  expect_true(e$call$formula[[2]][[2]] == "abc_t")
+  expect_true(all(!isTRUE(grepl("0", as.character(e$call$formula[[3]])))))
+  expect_true(all(!isTRUE(grepl("y", colnames(model.frame(e))))))
+
+  # Checking extra pieces in pblm
+  expect_true(!is.null(e$epb))
+  expect_equal(names(e$epb), c("mod1", "pred", "treatment", "data"))
+  expect_equal(e$epb$mod1, mod1)
+  expect_equal(e$epb$treatment, t)
+  expect_equal(e$epb$data, d)
+  expect_true(length(e$epb$pred) == nrow(d))
+
+  expect_error(pblm(mod1, c(0,0,0,0,0,1,1,1,1,2), d),
+               "must be indicator")
+  expect_error(pblm(lm(abc ~ ., data=d), t, d),
+               "only on control")
+  expect_error(pblm(mod1, t[1:9], d),
+               "same length")
 })
