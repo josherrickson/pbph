234763a3e934f9bb709d99fc9eea00eadb81be97
Josh Errickson <josh@errickson.net>
2015-08-03 13:40:30 -0400

    Using expect_equal instead of expect_true(all.equal

--- tests/testthat/test.bread_and_meat.R
+++ tests/testthat/test.bread_and_meat.R
@@ -23,14 +23,14 @@ test_that("bread", {
 
   #B21
   b21 <- bread21(eta=1, m$mod2$coef[1], resp, covs, m$pred, treatment)
-  expect_true(all.equal(b21, matrix(c(-8,-7567/285, -14, -5847/95,
-                                      -24, -26216/285 ), nrow=2),
-                        check.attributes=FALSE))
+  expect_equal(b21, matrix(c(-8,-7567/285, -14, -5847/95,
+                             -24, -26216/285 ), nrow=2),
+               check.attributes=FALSE)
 
   #B22
   b22 <- bread22(m$pred, treatment)
-  expect_true(all.equal(b22, matrix(c(4,23/3,23/3,251/9), nrow=2),
-                        check.attributes=FALSE))
+  expect_equal(b22, matrix(c(4,23/3,23/3,251/9), nrow=2),
+               check.attributes=FALSE)
 })
 
 test_that("meat", {
@@ -49,20 +49,20 @@ test_that("meat", {
 
   # M11
   m11 <- meat11(mod1, covs, treatment)
-  expect_true(all.equal(m11, matrix(c(8/3, 8, 16/3,
-                                      8, 224/9, 136/9,
-                                      16/3, 136/9, 104/9), nrow=3),
-                        check.attributes=FALSE))
+  expect_equal(m11, matrix(c(8/3, 8, 16/3,
+                             8, 224/9, 136/9,
+                             16/3, 136/9, 104/9), nrow=3),
+               check.attributes=FALSE)
 
   # M22
   # check this
   m22 <- meat22(1, m$mod2$coef[1], resp, m$pred, treatment)
-  expect_true(all.equal(m22, matrix(c(60.652767005232462338,
-                                      176.40659484969751247,
-                                      176.40659484969751247,
-                                      603.89350432611854558),
-                                    nrow=2),
-                        check.attributes=FALSE))
+  expect_equal(m22, matrix(c(60.652767005232462338,
+                             176.40659484969751247,
+                             176.40659484969751247,
+                             603.89350432611854558),
+                           nrow=2),
+               check.attributes=FALSE)
 })
 
 test_that("corrected var", {
