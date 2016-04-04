8c7ae66506b8bd126d294c8113fca5936af7ae60
Josh Errickson <josh@errickson.net>
2016-04-04 14:17:43 -0400

    Various fixes to logistic sim in prep for writing

--- simulations/logistic.R
+++ simulations/logistic.R
@@ -1,34 +1,33 @@
 n <- 100
 p <- 5
 pc <- .4
-informative <- .7
-true_t <- .5
+informative <- .4
+true_t <- .1
 sigma2 <- .3
-true_inter <- seq(-1,2,by = .5)
+true_inter <- seq(-.4,.4,by = .1)
 
-reps <- 100
+reps <- 1000
 bigsave <- epb:::makeSaveMatrix(c("truth", "coverage"),
                           reps = length(true_inter))
 for (j in 1:length(true_inter)) {
-  ti <- true_inter[j]
+  ti <- round(true_inter[j], 1)
   save <- vector(length = reps)
   for (i in 1:reps) {
+
     covs <- data.frame(matrix(rnorm(n*p), nrow = n))
     truebeta <- c(rnorm(round(informative*p)),
                   rep(0, round((1 - informative)*p)))
 
     treatment <- rep(0:1, c(n*pc, n*(1 - pc)))
 
-    noise <- runif(n, -.5, .5)
-    yc_un <- 1/(1 + exp(as.matrix(covs) %*% truebeta))
-    yt_un <- yc_un + true_t*treatment + ti*treatment*yc_un
-    resp <- round(ifelse(treatment == 1, yt_un, yc_un) + noise)
+    pr <- 1/(1 + exp(-as.matrix(covs) %*% truebeta)) + true_t*treatment + ti*treatment*yc_un
+    rep <- rbinom(n, 1, pmax(pmin(pr,1),0))
     d <- data.frame(y = resp, covs)
 
-    mod1 <- glm(y ~ ., data = d[treatment == 0,], family = 'binomial')
+    mod1 <- glm(y ~ ., data = d[treatment == 0,], family = 'binomial',)
     mod2 <- pblm(mod1, treatment, d)
 
-    save[j] <- 2*pt(abs(hypothesisTest(mod2, ti)), mod1$df.null, lower.tail = FALSE)
+    save[i] <- 2*pt(abs(hypothesisTest(mod2, ti)), mod1$df.null, lower.tail = FALSE)
   }
   coveraged <- table(save <= .05)["FALSE"]
   bigsave[j,] <- c(ti, coveraged)
