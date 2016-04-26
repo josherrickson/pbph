5a0df88c1d57a8524f03587d6eb45aeed7eeff97
Josh Errickson <josh@errickson.net>
2016-04-26 13:38:55 -0400

    Cleaned up glm sim.

--- simulations/logistic.R
+++ simulations/logistic.R
@@ -7,29 +7,28 @@ sigma2 <- .3
 true_inter <- seq(-.4,.4,by = .1)
 
 reps <- 100
-bigsave <- epb:::makeSaveMatrix(c("truth", "coverage", "finite",
-                                  "inf", "disjoint"),
+bigsave <- epb:::makeSaveMatrix(c("truth", "coverage", "inf", "disjoint"),
                                 reps = length(true_inter))
 for (j in 1:length(true_inter)) {
   ti <- round(true_inter[j], 1)
-  save <- epb:::makeSaveMatrix(c("covered", "type"), reps=reps)
+  save <- epb:::makeSaveMatrix(c("covered", "type"), reps = reps)
   for (i in 1:reps) {
 
     covs <- data.frame(matrix(rnorm(n*p), nrow = n))
-    truebeta <- c(rnorm(round(informative*p)),
-                  rep(0, round((1 - informative)*p)))
+    truebeta <- c(rnorm(round(informative * p)),
+                  rep(0, round((1 - informative) * p)))
 
-    treatment <- rep(0:1, c(n*pc, n*(1 - pc)))
+    treatment <- rep(0:1, c(n * pc, n * (1 - pc)))
 
     pr <- 1/(1 + exp(-as.matrix(covs) %*% truebeta))
-    pr <- pr + true_t*treatment + ti*treatment*pr
+    pr <- pr + true_t * treatment + ti * treatment * pr
     resp <- rbinom(n, 1, pmax(pmin(pr,1),0))
     d <- data.frame(y = resp, covs)
 
-    mod1 <- glm(y ~ ., data = d[treatment == 0,], family = binomial)
+    mod1 <- glm(y ~ ., data = d[treatment == 0, ], family = binomial)
     mod2 <- pblm(mod1, treatment, d)
 
-    ci <- confint(mod2, "pred", returnType = TRUE)
+    ci <- confint(mod2, "pred", returnShape = TRUE)
     type <- attr(ci, "type")
     if (type == "finite") {
       covered <- ci[1] < ti & ti < ci[2]
@@ -42,8 +41,8 @@ for (j in 1:length(true_inter)) {
     }
     type <- switch(type,
                    finite = 1,
-                   infinite = 2,
-                   disjoint = 3)
+                   infinite = 1,
+                   disjoint = 2)
 
 
     save[i,] <- c(covered, type)
@@ -52,8 +51,7 @@ for (j in 1:length(true_inter)) {
   coveraged <- table(save[,1])["1"]
   bigsave[j,] <- c(ti, coveraged,
                    sum(save[,2] == 1),
-                   sum(save[,2] == 2),
-                   sum(save[,2] == 3))
+                   sum(save[,2] == 2))
 
 }
 
