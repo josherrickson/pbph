426f73e2c390e7900f242493f121488976498836
Josh Errickson <josh@errickson.net>
2016-05-03 14:56:47 -0400

    Rebuilt logistic sim to cover proper parameter space.

--- simulations/logistic.R
+++ simulations/logistic.R
@@ -1,16 +1,27 @@
-n <- 100
-p <- 5
+n <- 1000
+p <- 13
 pc <- .4
 informative <- .4
-true_t <- .1
 sigma2 <- .3
-true_inter <- seq(-.3, -1, by = -.1)
+params <- data.frame(tau = rep(c(.25, .5, .75), each = 3),
+                     eta = c(-.5, -.75, -1,
+                             -.75, -1, -1.25,
+                             -1, -1.25, -1.5))
+params <- rbind(params, data.frame(tau = c(-.25, -.25,
+                                           .25, .25, .25,
+                                           .75, .75, .75,
+                                           1.25, 1.25),
+                                   eta = c(.25, -.25,
+                                           .25, -1.75, -2.25,
+                                           .25, -.25, -2.25,
+                                           -1.75, -2.25)))
 
-reps <- 100
-bigsave <- epb:::makeSaveMatrix(c("truth", "coverage", "cont", "disjoint"),
-                                reps = length(true_inter))
-for (j in 1:length(true_inter)) {
-  ti <- round(true_inter[j], 1)
+reps <- 1000
+bigsave <- epb:::makeSaveMatrix(c("true tau", "true eta", "coverage", "cont", "disjoint"),
+                                reps = nrow(params))
+for (j in 1:nrow(params)) {
+  tt <- params[j, 1]
+  ti <- params[j, 2]
   save <- epb:::makeSaveMatrix(c("covered", "type"), reps = reps)
   for (i in 1:reps) {
 
@@ -21,7 +32,7 @@ for (j in 1:length(true_inter)) {
     treatment <- rep(0:1, c(n * pc, n * (1 - pc)))
 
     pr <- 1/(1 + exp(-as.matrix(covs) %*% truebeta))
-    pr <- pr + true_t * treatment + ti * treatment * pr
+    pr <- pr + tt * treatment + ti * treatment * pr
     resp <- rbinom(n, 1, pmax(pmin(pr,1),0))
     d <- data.frame(y = resp, covs)
 
@@ -49,10 +60,18 @@ for (j in 1:length(true_inter)) {
 
   }
   coveraged <- table(save[,1])["1"]
-  bigsave[j,] <- c(ti, coveraged,
+  bigsave[j,] <- c(tt, ti, coveraged,
                    sum(save[,2] == 1),
                    sum(save[,2] == 2))
 
 }
 
 bigsave
+
+plot(NULL, xlim = c(-.5, 1.5), ylim = c(-2.5, 0.5), xlab = expression(tau), ylab = expression(eta))
+polygon(c(1, 1, -4), c(3, -2, 3), col = 'lightblue')
+polygon(c(0, 3, 0), c(0, -3, -3), col = 'lightgreen')
+polygon(c(0, 1, 1, 0), y = c(0, -1, -2, -1), col = 'red')
+perc <- paste0(round(bigsave[,3]/reps*100), "%")
+text(bigsave[,1:2], labels = perc)
+
