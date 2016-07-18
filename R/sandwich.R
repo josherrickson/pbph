71f89483bf9c6654bf818f13be9d1d4ecf834cb1
Josh Errickson <josh@errickson.net>
2016-07-18 10:21:39 -0400

    Citation for adjustment on meat

--- R/sandwich.R
+++ R/sandwich.R
@@ -11,6 +11,7 @@
 ##' @param cluster A variable identifying cluster.
 ##' @param ... Additional arguments to \code{meat}.
 ##' @return A covariance \code{matrix}.
+##' @seealso sandwich::sandwich
 ##' @export
 ##' @import sandwich
 sandwich <- function(x, bread. = bread, meat. = meat, cluster = NULL, ...) {
@@ -32,9 +33,20 @@ sandwich <- function(x, bread. = bread, meat. = meat, cluster = NULL, ...) {
 ##' @param x a fitted model object.
 ##' @param adjust See \code{sandwich::meat}. If a cluster is given,
 ##'   this argument is ignored as the adjustment is always used.
+##'   The adjustment is \deqn{m/(m - 1) * (n - 1)/(n - k),} where
+##'   \eqn{n} is the number of observations, \eqn{k} the number of
+##'   estimated parameters, and \eqn{m} the number of clusters. Note
+##'   that if there are no clusters (which can be thought of as
+##'   each observation being its own cluster), \eqn{m = n} and this
+##'   reduces to \deqn{n/(n-k),} the default in \code{sandwich::meat}.
+##'   See Cameron, A Colin, and Douglas L Miller. 2010. "Robust
+##'   Inference with Clustered Data." Working Papers, University
+##'   adjustment used when clustering.
+##'   of California, Department of Economics.
 ##' @param cluster A vector identifying cluster membership
 ##' @param ... Additional arguments to \code{sandwich::estfun}.
 ##' @return A meat \code{matrix}.
+##' @seealso sandwich::meat
 ##' @export
 ##' @import sandwich
 meat <- function(x, adjust = FALSE, cluster = NULL, ...) {
